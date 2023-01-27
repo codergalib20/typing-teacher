6832b47d5e93c18fd8d359e140e09469c58fcc6c
codergalib20 <coderboygalif@gmail.com>
2023-01-27 21:37:43 +0600

    commited

--- src/@core/layouts/components/shared-components/footer/FooterContent.tsx
+++ src/@core/layouts/components/shared-components/footer/FooterContent.tsx
@@ -23,24 +23,24 @@ const FooterContent = () => {
       </Typography>
       {hidden ? null : (
         <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', '& :not(:last-child)': { mr: 4 } }}>
-          <Link
+          {/* <Link
             target='_blank'
             href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free/blob/main/LICENSE'
           >
             MIT License
-          </Link>
-          <Link target='_blank' href='https://themeselection.com/'>
-            More Themes
+          </Link> */}
+          <Link target='_blank' href='https://codergalib.live/'>
+            About Asadullah
           </Link>
           <Link
             target='_blank'
-            href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free/blob/main/README.md'
+            href='https://github.com/codergalib20/typing-teacher/blob/main/README.md'
           >
             Documentation
           </Link>
           <Link
             target='_blank'
-            href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free/issues'
+            href='https://github.com/codergalib20/typing-teacher/issues'
           >
             Support
           </Link>
